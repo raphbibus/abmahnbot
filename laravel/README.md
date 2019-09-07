2c848cb2a445c526841445624a2b074a899cfa46
Ralph Cibis <raphbibus@rc-macbookpro.intern>
2019-09-07 13:36:53 +0200

    first commit

---
+++ laravel/README.md
@@ -0,0 +1,1 @@
+ssh -R abmahnung.serveo.net:80:localhost:8080 serveo.net
---
+++ laravel/routes/botman.php
@@ -0,0 +1,42 @@
+<?php
+use App\Http\Controllers\BotManController;
+use BotMan\BotMan\Messages\Attachments\Image;
+use BotMan\BotMan\Messages\Outgoing\OutgoingMessage;
+
+
+
+$botman = resolve('botman');
+
+$botman->hears('Hi', function ($bot) {
+    $bot->reply('Hello!');
+});
+$botman->hears('Start conversation', BotManController::class.'@startConversation');
+
+$botman->hears('{name} abmahnen', function ($bot, $name) {
+  Log::info($name);
+  $userId = '';
+  if (preg_match('/<@(.*?)>/', $name, $match) == 1) {
+    Log::info($match[1]);
+    $userId = $match[1];
+
+    $slack = new wrapi\slack\slack(config('services.slack.oauth'));
+
+    $user = $slack->users->info(array("user" => $userId));
+    $user = $user['user'];
+
+    $bot->reply($user['real_name'].' kriegt ne Abmahnung!');
+    $bot->say('Du wurdest abgemahnt!', $userId);
+
+  } else if ($name == "<!channel>") {
+
+    $bot->reply("Ihr kriegt alle ne Abmahnung!");
+
+  } else {
+
+    $bot->reply("Sprich halt bitte gescheit mit mir!");
+
+  }
+
+
+
+});
