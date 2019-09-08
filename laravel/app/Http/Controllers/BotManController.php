8ccc91c874079e789bb4039f6e2c56cbe2c117de
raphbibus <github@raphbib.us>
2019-09-08 13:20:19 +0200

    moved logic to botman controller

--- laravel/app/Http/Controllers/BotManController.php
+++ laravel/app/Http/Controllers/BotManController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use BotMan\BotMan\BotMan;
 use Illuminate\Http\Request;
 use App\Conversations\ExampleConversation;
+use Log;
+use wrapi\slack\slack as SlackSDK;
 
 class BotManController extends Controller
 {
@@ -35,8 +37,34 @@ class BotManController extends Controller
         $bot->startConversation(new ExampleConversation());
     }
 
-    public function abmahnConversation(BotMan $bot) {
-      
+    public function abmahnen(BotMan $bot, $name) {
+
+        $warner = $bot->getUser();
+
+        // Log::info($name);
+        $userId = '';
+        if (preg_match('/<@(.*?)>/', $name, $match) == 1) {
+            // Log::info($match[1]);
+            $userId = $match[1];
+
+            $slack = new SlackSDK(config('services.slack.oauth'));
+
+            $user = $slack->users->info(array("user" => $userId));
+            $user = $user['user'];
+
+            $bot->reply($user['real_name'].' kriegt ne Abmahnung!');
+            $bot->say("Du wurdest von {$warner->getFirstname()} {$warner->getLastname()} abgemahnt!", $userId);
+
+        } else if ($name == "<!channel>") {
+
+            $bot->reply("Ihr kriegt alle ne Abmahnung von {$warner->getFirstname()} {$warner->getLastname()}!");
+
+        } else {
+
+            $bot->reply("Sprich halt bitte gescheit mit mir {$warner->getFirstname()}! Du kannst mich mit \"@channel abmahnen\" oder \"@mention abmahnen\" benutzen.");
+
+        }
+
     }
 
 }
